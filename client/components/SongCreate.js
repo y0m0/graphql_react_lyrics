4548cfb7b8c7af72422aecb3d30738496deafdbd
y0m0 <lixi.daniele@gmail.com>
2018-04-17 11:32:05 +0200

    Add input and save what's written in the state

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { title: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <h3>Create a New Song</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
         </form>
       </div>
     );
