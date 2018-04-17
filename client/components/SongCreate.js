eb2f459f79bb703c6385a84470f95cebcbd396be
y0m0 <lixi.daniele@gmail.com>
2018-04-17 11:25:25 +0200

    Create a new route and component to add new songs

---
+++ client/components/SongCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
