4510dc0a5945a2395f75de8da1e07c09826c4072
y0m0 <lixi.daniele@gmail.com>
2018-04-17 14:35:49 +0200

    Add back button to SongCreate page and "add" button to SongList page

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
@@ -18,9 +19,17 @@ class SongList extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
+      <div>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+        <Link
+          to="/songs/new"
+          className="btn-floating btn-medium red right"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     );
   }
 }
