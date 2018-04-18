afe9bc36fa6e13ec2decace0bf84898a31b97916
y0m0 <lixi.daniele@gmail.com>
2018-04-18 22:21:49 +0200

    Add a LyricList component under SongDetails

---
+++ client/components/LyricList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LyricList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          Lyric List
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default LyricList;
--- client/components/SongDetails.js
+++ client/components/SongDetails.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 // graphql query
 import getSongById from '../queries/getSongById';
@@ -18,6 +19,7 @@ class SongDetails extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
