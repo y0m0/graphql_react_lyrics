384476c10fa572336b451bbc2eb6ce90c33eac05
y0m0 <lixi.daniele@gmail.com>
2018-04-18 21:52:27 +0200

    Connect input field to component state

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Add a Lyric</label>
-          <input />
+          <input
+            value={this.state.content}
+            onChange={event => this.setState({ content: event.target.value })}
+          />
         </form>
       </div>
     );
