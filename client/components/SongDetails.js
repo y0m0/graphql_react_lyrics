90f46f5dfdffabdbe27377f08540524c42d1d153
y0m0 <lixi.daniele@gmail.com>
2018-04-18 18:36:58 +0200

    Show the song title that comes from the gql query in the header - render the full page only when the query is finished, otherwise show a loading screen

--- client/components/SongDetails.js
+++ client/components/SongDetails.js
@@ -6,14 +6,21 @@ import getSongById from '../queries/getSongById';
 
 class SongDetails extends Component {
   render() {
+    const { song } = this.props.data;
+
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        <h3>Song Details</h3>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
+// this is needed because the song ID is a required argument of the query
+// and we have to pass it from the props to the graphql query
 export default graphql(getSongById, {
   options: (props) => { return { variables: { id: props.params.id } } }
 })(SongDetails);
