bc15006a43dd71c988a8a8ff0caf9ecb0b261d8e
y0m0 <lixi.daniele@gmail.com>
2018-04-18 23:03:31 +0200

    add a Like icon to the lyrics

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log(id)
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(lyric => {
       return (
         <li key={lyric.id} className="collection-item">
           {lyric.content}
+          <i
+            onClick={() => this.onLike(lyric.id)}
+            className="material-icons"
+          >
+            thumb_up
+          </i>
         </li>
       );
     });
